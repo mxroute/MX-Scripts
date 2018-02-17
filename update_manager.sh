30327585918c86ab3a26c4dd7bf61771d0f20172
Jarland Donnell <jarland@gmail.com>
2018-02-17 05:47:34 +0000

    Update 'update_manager.sh'

--- update_manager.sh
+++ update_manager.sh
@@ -1,3 +1,5 @@
 # Script to update manager theme on nodes
+git -C /usr/local/cpanel/base/frontend/manager reset --hard
 git -C /usr/local/cpanel/base/frontend/manager pull
+git -C /usr/local/cpanel/base/webmail/manager reset --hard
 git -C /usr/local/cpanel/base/webmail/manager pull
