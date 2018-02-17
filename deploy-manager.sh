d1f15264d4bb4d27fa6508b572a99ba0e414e37a
Jarland Donnell <jarland@gmail.com>
2018-02-17 05:16:11 +0000

    Add 'deploy-manager.sh'

---
+++ deploy-manager.sh
@@ -0,0 +1,4 @@
+rm -rf /usr/local/cpanel/base/frontend/manager
+git clone https://git.mxcloud.io/jarland/MXroute_Manager.git /usr/local/cpanel/base/frontend/manager
+rm -rf /usr/local/cpanel/base/webmail/manager
+git clone https://git.mxcloud.io/jarland/MXroute_Manager_Webmail.git /usr/local/cpanel/base/webmail/manager
