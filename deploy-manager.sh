b24b7585fc037c37c78752d1f44c3f58e6dbb57f
Jarland Donnell <jarland@gmail.com>
2018-02-17 05:21:02 +0000

    Update 'deploy-manager.sh'

--- deploy-manager.sh
+++ deploy-manager.sh
@@ -1,3 +1,5 @@
+# Script for initial deployment of manager theme from git
+
 rm -rf /usr/local/cpanel/base/frontend/manager
 git clone https://git.mxcloud.io/jarland/MXroute_Manager.git /usr/local/cpanel/base/frontend/manager
 rm -rf /usr/local/cpanel/base/webmail/manager
