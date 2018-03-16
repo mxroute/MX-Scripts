ec72bd98b54ba7d206487c939e5b86057e85452d
mxroute <37432698+mxroute@users.noreply.github.com>
2018-03-16 00:53:21 -0500

    Moving to github

--- deploy-manager.sh
+++ deploy-manager.sh
@@ -1,6 +1,6 @@
 # Script for initial deployment of manager theme from git
 
 rm -rf /usr/local/cpanel/base/frontend/manager
-git clone https://git.mxcloud.io/jarland/MXroute_Manager.git /usr/local/cpanel/base/frontend/manager
+git clone https://github.com/mxroute/MXroute_Manager /usr/local/cpanel/base/frontend/manager
 rm -rf /usr/local/cpanel/base/webmail/manager
-git clone https://git.mxcloud.io/jarland/MXroute_Manager_Webmail.git /usr/local/cpanel/base/webmail/manager
+git clone https://github.com/mxroute/MXroute_Manager /usr/local/cpanel/base/webmail/manager
