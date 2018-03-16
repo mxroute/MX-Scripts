f6ea6e4f1f0157f535190b615520bb249543aaca
mxroute <37432698+mxroute@users.noreply.github.com>
2018-03-16 00:57:40 -0500

    Oops, let's not delete webmail

--- deploy-manager.sh
+++ deploy-manager.sh
@@ -2,5 +2,5 @@
 
 rm -rf /usr/local/cpanel/base/frontend/manager
 git clone https://github.com/mxroute/MXroute_Manager /usr/local/cpanel/base/frontend/manager
-rm -rf /usr/local/cpanel/base/webmail/manager
-git clone https://github.com/mxroute/MXroute_Manager /usr/local/cpanel/base/webmail/manager
+#rm -rf /usr/local/cpanel/base/webmail/manager
+#git clone https://github.com/mxroute/MXroute_Manager /usr/local/cpanel/base/webmail/manager
