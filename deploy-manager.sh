010ba24c8bee645c1297fd54452e98fa2b353e3b
mxroute <37432698+mxroute@users.noreply.github.com>
2018-08-08 15:02:09 -0500

    Update deploy-manager.sh

--- deploy-manager.sh
+++ deploy-manager.sh
@@ -1,6 +1,5 @@
 # Script for initial deployment of manager theme from git
 
 rm -rf /usr/local/cpanel/base/frontend/manager
-git clone https://github.com/mxroute/MXroute_Manager /usr/local/cpanel/base/frontend/manager
-#rm -rf /usr/local/cpanel/base/webmail/manager
-#git clone https://github.com/mxroute/MXroute_Manager /usr/local/cpanel/base/webmail/manager
+git clone https://github.com/mxroute/manager2 /usr/local/cpanel/base/frontend/manager
+cp -R /usr/local/cpanel/base/webmail/paper_lantern /usr/local/cpanel/base/webmail/manager
