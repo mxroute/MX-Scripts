50563dcc51125307a986707024e2581853237502
mxroute <37432698+mxroute@users.noreply.github.com>
2018-12-10 01:20:59 -0600

    Update update_manager.sh
    Updating webmail via git not currently part of plan

--- update_manager.sh
+++ update_manager.sh
@@ -1,5 +1,5 @@
 # Script to update manager theme on nodes
 /usr/local/cpanel/3rdparty/lib/path-bin/git -C /usr/local/cpanel/base/frontend/manager reset --hard
 /usr/local/cpanel/3rdparty/lib/path-bin/git -C /usr/local/cpanel/base/frontend/manager pull
-/usr/local/cpanel/3rdparty/lib/path-bin/git -C /usr/local/cpanel/base/webmail/manager reset --hard
-/usr/local/cpanel/3rdparty/lib/path-bin/git -C /usr/local/cpanel/base/webmail/manager pull
+#/usr/local/cpanel/3rdparty/lib/path-bin/git -C /usr/local/cpanel/base/webmail/manager reset --hard
+#/usr/local/cpanel/3rdparty/lib/path-bin/git -C /usr/local/cpanel/base/webmail/manager pull
