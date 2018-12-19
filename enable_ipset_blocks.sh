baac7134f12da8eef3a3e47d313a423aa5cc9eca
root <root@eagle.mxlogin.com>
2018-12-19 00:56:02 -0600

    Adding ipset blocks

---
+++ enable_ipset_blocks.sh
@@ -0,0 +1,1 @@
+echo "0 */12 * * * /bin/bash /root/MX-Scripts/ipset_blocks.sh >/dev/null 2>&1" >> /var/spool/cron/root
---
+++ ipset_blocks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ipset -q flush ipsum
+ipset -q create ipsum hash:net
+for ip in $(curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1); do ipset add ipsum $ip; done
+iptables -I INPUT -m set --match-set ipsum src -j DROP
