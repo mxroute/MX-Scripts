f89d0fe9f4bb755f08506d13e7d6b688f8bb564c
mxroute <37432698+mxroute@users.noreply.github.com>
2019-06-14 09:38:56 -0500

    Update optimize_errors.sh
    Added clarity to rejected UDID error

--- optimize_errors.sh
+++ optimize_errors.sh
@@ -1,2 +1,2 @@
 # Optimize error on subdomain random string failure
-/usr/bin/sed -i 's/an invalid subdomain was specified/an error occurred. The developers have been notified./g' /usr/local/cpanel/Cpanel/SubDomain.pm
+/usr/bin/sed -i 's/an invalid subdomain was specified/an error occurred. Please go back, refresh the page, and try again./g' /usr/local/cpanel/Cpanel/SubDomain.pm
